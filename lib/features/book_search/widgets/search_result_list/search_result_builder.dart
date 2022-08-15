44d21a3878ed54a277b9055a8a80b2bc6772cd82
bijan <riobijanero@gmail.com>
2022-08-15 17:27:56 +0200

    split up up buildList into smaller methods

--- lib/features/book_search/widgets/search_result_list/search_result_builder.dart
+++ lib/features/book_search/widgets/search_result_list/search_result_builder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:provider/provider.dart';
 
 import 'package:reos_challenge/shared/providers/book_provider.dart';
@@ -11,57 +10,70 @@ import 'package:reos_challenge/features/book_search/widgets/search_result_list/s
 class SearchResultBuilder {
   static Widget buildList(
     BuildContext context,
-    FloatingSearchBarController searchbarController,
+    Function onResultselected,
   ) {
-    if (searchbarController.query.isNotEmpty) {
-      final List<Widget> bookResultList = context.read<BookProvider>().bookSearchResultList.map((book) {
-        return Column(
-          children: [
-            SearchResultListItem(
-              book: book,
-              onTab: () {
-                searchbarController.query = '';
-                searchbarController.close();
-                presentDetailScreen(context: context, book: book);
-              },
-            ),
-            Divider(color: Theme.of(context).dividerColor),
-          ],
-        );
-      }).toList();
+    final List<Widget> bookResultList = _buildBookResultList(context, onResultselected);
+    final List<Widget> authorResultList = _buildAuthorResultList(context, onResultselected);
 
-      final List<Widget> authorResultList = context.read<BookProvider>().authorSearchResultList.map((book) {
-        return Column(
-          children: [
-            SearchResultListItem(
-              book: book,
-              onTab: () {
-                searchbarController.query = '';
-                searchbarController.close();
-                presentDetailScreen(context: context, book: book);
-              },
-            ),
-            Divider(color: Theme.of(context).dividerColor),
-          ],
-        );
-      }).toList();
-
-      return Material(
-        color: Theme.of(context).colorScheme.background,
-        elevation: 2.0,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            key: const Key('searchResultDropdownList'),
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[if (bookResultList.isNotEmpty) const ListHeader(title: 'Books')] +
-                bookResultList +
-                [if (authorResultList.isNotEmpty) const ListHeader(title: 'Authors')] +
-                authorResultList,
-          ),
+    return Material(
+      key: const Key('searchResultKey'),
+      color: Theme.of(context).colorScheme.background,
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          key: const Key('searchResultDropdownList'),
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[if (bookResultList.isNotEmpty) const ListHeader(title: 'Books')] +
+              bookResultList +
+              [if (authorResultList.isNotEmpty) const ListHeader(title: 'Authors')] +
+              authorResultList,
         ),
+      ),
+    );
+  }
+
+  static List<Widget> _buildBookResultList(
+    BuildContext context,
+    Function onResultTapped,
+  ) {
+    final List<Widget> bookResultList = context.read<BookProvider>().bookSearchResultList.map((book) {
+      return Column(
+        children: [
+          SearchResultListItem(
+            book: book,
+            onTab: () {
+              onResultTapped();
+              presentDetailScreen(context: context, book: book);
+            },
+          ),
+          Divider(color: Theme.of(context).dividerColor),
+        ],
       );
-    }
-    return const SizedBox();
+    }).toList();
+
+    return bookResultList;
+  }
+
+  static List<Widget> _buildAuthorResultList(
+    BuildContext context,
+    Function onResultTapped,
+  ) {
+    final List<Widget> authorResultList = context.read<BookProvider>().authorSearchResultList.map((book) {
+      return Column(
+        children: [
+          SearchResultListItem(
+            book: book,
+            onTab: () {
+              onResultTapped();
+              presentDetailScreen(context: context, book: book);
+            },
+          ),
+          Divider(color: Theme.of(context).dividerColor),
+        ],
+      );
+    }).toList();
+
+    return authorResultList;
   }
 }
